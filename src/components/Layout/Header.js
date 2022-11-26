40e4895ae8bd33f2b40a176a09213b886f160622
samithperera94 <samiththilanka94@gmail.com>
2022-11-26 11:29:21 +0530

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = props => {
+    return
+    <>
+        <header>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+        </header>  
+        <div>
+            <img />
+        </div>  
+    </>
+};
+
+export default Header;
